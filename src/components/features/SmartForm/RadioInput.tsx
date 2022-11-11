c0b4590c9b82d299cea996b530c1a2d5bedabfaa
Lolikana <thomas.guiart@gmail.com>
2022-11-11 10:37:53 +0900

    reset and required

--- src/components/features/SmartForm/RadioInput.tsx
+++ src/components/features/SmartForm/RadioInput.tsx
@@ -13,6 +13,7 @@ const RadioInput = ({ register, value, name, classRadio, id, onChange }: TRadio)
         id={id}
         {...register}
         className={`float-left mr-2 mt-1 h-4 w-4 cursor-pointer appearance-none rounded-full border border-gray-300 bg-white bg-contain bg-center bg-no-repeat align-top transition duration-200 focus:outline-none ${classRadio}`}
+        required
       />
       <label htmlFor={id}>{value}</label>
     </div>
