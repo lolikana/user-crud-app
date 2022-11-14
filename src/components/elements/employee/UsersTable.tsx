be6ef7f502d016b26ba3b7732262a61725c205a3
Lolikana <thomas.guiart@gmail.com>
2022-11-14 13:51:24 +0900

    body user table

--- src/components/elements/employee/UsersTable.tsx
+++ src/components/elements/employee/UsersTable.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { FC, MouseEventHandler } from 'react';
 
 import { BodyTable, HeadTable, Table } from '@/components/features/Table';
 
@@ -6,30 +6,29 @@ const userTitle = {
   col_1: 'Name',
   col_2: 'Phone',
   col_3: 'Email',
-  col_4: 'Role',
-  col_5: 'Created　On',
+  col_4: 'Created On',
+  col_5: 'Role',
   col_6: 'Status'
 };
+
 const usersInfos = [
   {
-    user1: {
-      name: 'Tom Gr',
-      phone: '000-0000-0000',
-      email: 'tom.gr@maeda-g.co.jp',
-      role: 'Admin',
-      createdOn: '2022-11-11',
-      status: 'Active'
-    }
+    id: 1,
+    name: 'Tom Gr',
+    phone: '000-0000-0000',
+    email: 'tom.gr@maeda-g.co.jp',
+    role: 'Admin',
+    createdOn: '2022-11-11',
+    status: 'Active'
   },
   {
-    user2: {
-      name: 'Tom Gr',
-      phone: '000-0000-0000',
-      email: 'tom.gr@maeda-g.co.jp',
-      role: 'Admin',
-      createdOn: '2022-11-11',
-      status: 'Active'
-    }
+    id: 2,
+    name: 'Tom Gr',
+    phone: '000-0000-0000',
+    email: 'tom.gr@maeda-g.co.jp',
+    role: 'Admin',
+    createdOn: '2022-11-11',
+    status: 'Active'
   }
 ];
 
@@ -37,7 +36,35 @@ const UsersTable = () => {
   return (
     <Table>
       <HeadTable colTitle={userTitle} />
-      <BodyTable rowsInfos={usersInfos} />
+      <BodyTable>
+        {usersInfos.map(datas => (
+          <tr
+            key={datas.id}
+            className="border-b bg-white dark:border-gray-700 dark:bg-gray-900"
+          >
+            <th
+              scope="row"
+              className="whitespace-nowrap py-4 px-6 font-medium text-gray-900 dark:text-white"
+            >
+              {datas.name}
+            </th>
+            <td className="py-4 px-6">{datas.phone}</td>
+            <td className="py-4 px-6">{datas.email}</td>
+            <td className="py-4 px-6">{datas.createdOn}</td>
+            <td className="py-4 px-6">{datas.role}</td>
+            <td className="py-4 px-6">{datas.status}</td>
+            <td className="py-4 px-6">
+              <span className="cursor-pointer font-medium text-blue-600 hover:underline dark:text-blue-500">
+                Edit
+              </span>{' '}
+              /{' '}
+              <span className="cursor-pointer font-medium text-red-600 hover:underline dark:text-red-500">
+                Delete
+              </span>
+            </td>
+          </tr>
+        ))}
+      </BodyTable>
     </Table>
   );
 };
