d64d247fdf1b5980411f9722ec5f874b0ea7df72
Lolikana <thomas.guiart@gmail.com>
2022-11-15 15:32:24 +0900

    CRUD mongodb

--- src/database/connect.ts
+++ src/database/connect.ts
@@ -1,15 +1,11 @@
 import mongoose from 'mongoose';
 
-const MONGO_URl =
-  'mongodb+srv://nextjs-crud-admin:TDkvLv0Z30We8Rdo@nextjs-crud-users.kilq1hw.mongodb.net/?retryWrites=true&w=majority';
-
 const connectMongo = async () => {
   try {
-    const { connection } = await mongoose.connect(MONGO_URl);
-
+    const { connection } = await mongoose.connect(process.env.MONGODB_URI!);
     if (connection.readyState === 1) return console.log('Database Connected');
-  } catch (err) {
-    return Promise.reject(err);
+  } catch (error) {
+    return Promise.reject(error);
   }
 };
 
