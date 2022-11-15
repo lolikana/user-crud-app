8346038b474285a8406ea00f6573832fe98b40cf
Lolikana <thomas.guiart@gmail.com>
2022-11-15 15:49:19 +0900

    CRUD by [userId]

--- src/pages/api/users/[userId].ts
+++ src/pages/api/users/[userId].ts
@@ -1,27 +1,26 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 import connectMongo from '@/database/connect';
-import { getUsers, postUser } from '@/database/controller';
+import { deleteUser, getUser, upadteUser } from '@/database/controller';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   connectMongo().catch(() => res.status(405).json({ error: 'Error in the connection' }));
 
-  // type of request
   const { method } = req;
 
   switch (method) {
     case 'GET':
-      getUsers(req, res);
+      getUser(req, res);
       break;
-    case 'POST':
-      postUser(req, res);
+    case 'PUT':
+      upadteUser(req, res);
+      break;
+    case 'DELETE':
+      deleteUser(req, res);
       break;
 
     default:
       res.setHeader('Allow', ['GET', 'POST', 'PUT', 'DELETE']);
       res.status(405).end(`Method ${method} not allowed`);
   }
-  // [GET, POST, PUT, DELETE]
-
-  //   res.status(200).json({ name: 'John Doe' });
 }
